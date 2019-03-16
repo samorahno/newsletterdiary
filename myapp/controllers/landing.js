1ad9a8d68264f9f06280cc51c94f8d4c0f7da38e
Samson Abosede <samabos007@gmail.com>
2019-03-16 16:39:50 +0100

    Create, Retrieve, update lead

--- myapp/controllers/landing.js
+++ myapp/controllers/landing.js
@@ -7,6 +7,49 @@ exports.get_landing = function(req, res, next) {
 exports.submit_lead = function(req, res, next) {
   return models.Lead.create({
     email: req.body.email,
-  }).then(lead => res.redirect('/'));
+  }).then(lead => res.redirect('/leads'));
   
 };
+
+exports.show_leads = function(req, res, next) {
+  return models.Lead.findAll()
+  .then(result => res.render('landing', {
+    page_title: 'List of Subscribers',
+    leads: result,
+  }));
+}
+
+exports.show_lead = function(req, res, next){
+  return models.Lead.findOne({
+    where: {
+      id: req.params.leadid,
+    } 
+  })
+  .then(lead => res.render('lead', {
+    title: 'Lead Details',
+    lead: lead,
+  }));
+}
+
+exports.show_edit_lead = function(req, res, next){
+  return models.Lead.findOne({
+    where: {
+      id: req.params.leadid,
+    } 
+  })
+  .then(lead => res.render('lead_edit', {
+    title: 'Edit Lead',
+    lead: lead,
+  }));
+}
+
+exports.edit_lead = function(req, res, next){
+  return models.Lead.update({
+    email: req.body.email
+  }, {
+    where: {
+      id: req.params.leadid,
+    }
+  }).then(result => res.redirect('/lead/' + req.params.leadid));
+}
+
