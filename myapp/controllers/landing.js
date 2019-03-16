96b2e59d4996d5a1406cb15ae093c908bd858979
Samson Abosede <samabos007@gmail.com>
2019-03-16 11:11:13 +0100

    Postgres and sequelize setup

--- myapp/controllers/landing.js
+++ myapp/controllers/landing.js
@@ -1,3 +1,8 @@
-exports.index = function(req, res, next) {
-  res.render("index", { title: "Express" });
+exports.get_landing = function(req, res, next) {
+  res.render("landing", { title: "NewsLetter Diary" });
+};
+
+exports.submit_lead = function(req, res, next) {
+  console.log(req.body.email);
+  res.redirect('/');
 };
